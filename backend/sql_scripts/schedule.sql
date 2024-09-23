a0b7fa64cd09680319ca03b055531fb25f3c45aa
ChyeShaoXuan <142379429+ChyeShaoXuan@users.noreply.github.com>
2024-09-23 17:17:47 +0800

    further changes due to conflict with composite primary key

--- backend/sql_scripts/schedule.sql
+++ backend/sql_scripts/schedule.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS schedule;
 
 CREATE TABLE schedule (
+    Schedule_ID PRIMARY KEY INT NOT NULL, --Schedule ID for staff
     Staff_ID INT NOT NULL,  -- Staff ID for staff
     Date DATE NOT NULL,  -- Date of WFH arrangement
-    Timing VARCHAR(50) NOT NULL, -- AM/PM/Whole day/in-office
-    PRIMARY KEY (Staff_ID,Date) -- composite primary key to uniquely identify schedule based on date & am/pm
+    Timing VARCHAR(50) NOT NULL -- AM/PM/Whole day/in-office
 );
