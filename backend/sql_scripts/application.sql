ff3c757688a901b2753c4227d05430018730447b
ChyeShaoXuan <142379429+ChyeShaoXuan@users.noreply.github.com>
2024-09-23 17:05:44 +0800

    changes to syntax for application script

--- backend/sql_scripts/application.sql
+++ backend/sql_scripts/application.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS application
+DROP TABLE IF EXISTS application;
 
 CREATE TABLE application (
-    Application_ID INT PRIMARY KEY; --
+    Application_ID INT PRIMARY KEY, --
     Request_Type VARCHAR(20), -- Ad hoc or recurring basis
     Starting_Date DATE NOT NULL, -- starting date of WFH arrangement
     End_Date DATE NOT NULL, -- end date of WFH arrangement
@@ -9,5 +9,5 @@ CREATE TABLE application (
     Timing VARCHAR(50) NOT NULL, -- AM/PM/Whole day
     Staff_ID INT NOT NULL,  -- Staff ID for staff
     Status VARCHAR(50) NOT NULL, -- status of application (pending/approved/rejected)
-    CONSTRAINT fk_reporting_manager FOREIGN KEY (Staff_ID) REFERENCES schedule(Staff_ID), -- foreign key referencing staff id in schedule table
+    CONSTRAINT fk_reporting_manager FOREIGN KEY (Staff_ID) REFERENCES schedule(Staff_ID) -- foreign key referencing staff id in schedule table
 ); 
