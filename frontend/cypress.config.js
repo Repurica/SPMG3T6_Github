4a7b3f98d498f0db2d1b0360a3f6c9db901e6e28
Repurica <65117070+Repurica@users.noreply.github.com>
2024-10-20 21:52:13 +0800

    ignore Cannot find module 'devextreme/dist/css/dx.material.blue.light.css'

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("cypress");
 
+try {
+  require('devextreme/dist/css/dx.material.blue.light.css');
+} catch (error) {
+  console.warn("Optional module 'devextreme/dist/css/dx.material.blue.light.css' not found. Continuing without it.");
+}
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
